2df0cb637b8a74cb3e4718c4b68781985e5c259a
Kahsolt <kahsolt@qq.com>
2024-01-12 20:13:59 +0800

    add vis dataset

--- utils.py
+++ utils.py
@@ -7,8 +7,11 @@ from pathlib import Path
 from typing import *
 
 from tqdm import tqdm
-from PIL import Image
+from PIL import Image, ImageFilter
+from PIL.Image import Image as PILImage
+from skimage.metrics import *
 import numpy as np
+from numpy import ndarray
 import matplotlib.pyplot as plt
 
 BASE_PATH = Path(__file__).parent
@@ -19,6 +22,8 @@ IMG_PATH = BASE_PATH / 'img' ; IMG_PATH.mkdir(exist_ok=True)
 OUT_PATH = BASE_PATH / 'output' ; OUT_PATH.mkdir(exist_ok=True)
 TRUTH_FILE = OUT_PATH / 'result-ref.txt'
 RESULT_FILE = OUT_PATH / 'result.txt'
+
+npimg = ndarray
 
 
 def get_test_fps(dp:Path=DATA_PATH):
@@ -47,3 +52,39 @@ def load_db(fp:Path) -> Dict[str, Any]:
 def save_db(db:Dict[str, Any], fp:Path):
   with open(fp, 'w', encoding='utf-8') as fh:
     json.dump(db, fh, indent=2, ensure_ascii=False)
+
+
+def load_img(fp:Path) -> PILImage:
+  return Image.open(fp).convert('RGB')
+
+def pil_to_npimg(img:PILImage) -> npimg:
+  return np.asarray(img, dtype=np.uint8)
+
+def npimg_to_pil(im:npimg) -> PILImage:
+  assert im.dtype in [np.uint8, np.float32]
+  if im.dtype == np.float32:
+    assert 0.0 <= im.min() and im.max() <= 1.0
+  return Image.fromarray(im)
+
+def to_ch_avg(x:ndarray) -> ndarray:
+  return np.tile(x.mean(axis=-1, keepdims=True), (1, 1, 3))
+
+def to_gray(im:npimg) -> npimg:
+  return pil_to_npimg(npimg_to_pil(im).convert('L'))
+
+def minmax_norm(dx:ndarray, vmin:int=None, vmax:int=None) -> npimg:
+  vmin = vmin or dx.min()
+  vmax = vmax or dx.max()
+  out = (dx - vmin) / (vmax - vmin)
+  return (out * 255).astype(np.uint8)
+
+def npimg_diff(x:npimg, y:npimg) -> ndarray:
+  return x.astype(np.int16) - y.astype(np.int16)
+
+def npimg_abs_diff(x:npimg, y:npimg, name:str=None) -> npimg:
+  d = np.abs(npimg_diff(x, y))
+  if name:
+    print(f'[{name}]')
+    print('  Linf:', d.max() / 255)
+    print('  L1:',  d.mean() / 255)
+  return d
---
+++ vis.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# Author: Armit
+# Create Time: 2023/10/26
+
+from argparse import ArgumentParser
+from numpy import fft
+import seaborn as sns
+from utils import *
+
+
+def stats_hw():
+  fp = IMG_PATH / 'img_size.png'
+  if fp.exists(): return
+
+  ws, hs = [], []
+  fps = list(DATA_PATH.iterdir())
+  for fp in tqdm(fps):
+    img = load_img(fp)
+    w, h = img.size
+    ws.append(w)
+    hs.append(h)
+  plt.scatter(ws, hs, s=1)
+  plt.xlabel('width')
+  plt.ylabel('hieght')
+  plt.savefig(fp, dpi=600)
+
+  print('min(width):',  min(ws))
+  print('min(height):', min(hs))
+  print('max(width):',  max(ws))
+  print('max(height):', max(hs))
+
+
+def plot_hi_lo(img:PILImage):
+  img_lo = img.filter(ImageFilter.GaussianBlur(3))
+  im_lo = pil_to_npimg(img_lo)
+  im = pil_to_npimg(img)
+  im_hi = minmax_norm(npimg_diff(im, im_lo))
+
+  plt.clf()
+  plt.subplot(1, 3, 1) ; plt.axis('off') ; plt.title('X')    ; plt.imshow(im)
+  plt.subplot(1, 3, 2) ; plt.axis('off') ; plt.title('X_lo') ; plt.imshow(im_lo)
+  plt.subplot(1, 3, 3) ; plt.axis('off') ; plt.title('X_hi') ; plt.imshow(im_hi)
+  plt.show()
+
+
+def plot_fft(img:PILImage):
+  img_grey = img.convert('L')
+  amp = np.log(np.abs(fft.fftshift(fft.fft2(img_grey))))
+  im = pil_to_npimg(img).astype(np.float32) / 255.0
+  freq = np.stack([fft.fftshift(fft.fft2(im[:, :, i])) for i in range(im.shape[-1])], axis=-1)
+
+  r = 30
+  H, W, C = freq.shape
+  Hc, Wc = H // 2, W // 2
+  freq_lo = np.zeros_like(freq)
+  freq_lo[Hc-r:Hc+r, Wc-r:Wc+r, :] = freq[Hc-r:Hc+r, Wc-r:Wc+r, :]
+  freq_hi = freq - freq_lo
+  im_lo = np.stack([fft.ifft2(fft.ifftshift(freq_lo[:, :, i])) for i in range(C)], axis=-1)
+  im_hi = np.stack([fft.ifft2(fft.ifftshift(freq_hi[:, :, i])) for i in range(C)], axis=-1)
+  im_lo = np.abs(im_lo)
+  im_hi = np.abs(im_hi)
+
+  plt.clf()
+  plt.subplot(2, 2, 1) ; plt.imshow(im)    ; plt.title('X')
+  plt.subplot(2, 2, 2) ; sns.heatmap(amp, cbar=True) ; plt.title('freq')
+  plt.subplot(2, 2, 3) ; plt.imshow(im_lo) ; plt.title('low')
+  plt.subplot(2, 2, 4) ; plt.imshow(im_hi) ; plt.title('high')
+  plt.show()
+
+
+if __name__ == '__main__':
+  parser = ArgumentParser()
+  parser.add_argument('--hw',  action='store_true')
+  parser.add_argument('--hf',  action='store_true')
+  parser.add_argument('--fft', action='store_true')
+  args = parser.parse_args()
+
+  if args.hw:
+    stats_hw()
+
+  if args.hf:
+    try:
+      for fp in DATA_PATH.iterdir():
+        img = load_img(fp)
+        plot_hi_lo(img)
+    except KeyboardInterrupt:
+      pass
+
+  if args.fft:
+    try:
+      for fp in DATA_PATH.iterdir():
+        img = load_img(fp)
+        plot_fft(img)
+    except KeyboardInterrupt:
+      pass
